2c6d72973e9baec8426c8778cb006d85c64a2374
Artem <skickoartem74@gmail.com>
2025-01-27 17:10:49 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // write code here
+const list = document.body.querySelectorAll('li');
+
+const employees = [];
+
+for (let i = 0; i < list.length; i++) {
+  const employeeName = list[i].innerHTML.trim();
+  const salary = parseInt(
+    list[i].getAttribute('data-salary').replace(/[$,]/g, ''),
+  );
+
+  employees.push({ employeeName, salary });
+}
+
+function sortList(employeesList) {
+  return employeesList.sort((a, b) => b.salary - a.salary);
+}
+
+const sorted = sortList(employees);
+
+for (let i = 0; i < sorted.length; i++) {
+  list[i].innerHTML = sorted[i].employeeName;
+}
